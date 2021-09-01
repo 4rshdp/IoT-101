f46150a6e6f35dc276aab80f12af14d6dc613930
arshsabudh <arshdeep.s@sabudh.org>
2021-09-01 06:16:51 +0530

    handson01

---
+++ Module01-IoTandEmbeddedComputing/01-IntroductionToKitAndInitialSetup/blinking_led_ESP-32/blinking_led_ESP-32.ino
@@ -0,0 +1,15 @@
+
+const byte led_GPIO = 19;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led_GPIO, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(led_GPIO, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(led_GPIO, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
