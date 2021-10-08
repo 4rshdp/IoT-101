ee5662fea7fb6df781f97ab7c296530fc7b82fee
arshsabudh <arshdeep.s@sabudh.org>
2021-10-08 07:27:58 +0530

    .

--- Module01-IoTandEmbeddedComputing/08 Timers & PWM/Timers___PWM/Timers___PWM.ino
+++ Module01-IoTandEmbeddedComputing/08 Timers & PWM/Timers___PWM/Timers___PWM.ino
@@ -2,24 +2,25 @@
 
 
 
-hw_timer_t * timer = NULL;
-
-void IRAM_ATTR onTimer(){
-  Serial.println(String(millis()));
-}
-
-void setup() {
-  Serial.begin(115200);
-
-  Serial.println("start timer ");
-  timer = timerBegin(0, 80, true);  // timer 0, prescaler is dividing by 80 giving time perod of 1 micro second., count up
-  timerAttachInterrupt(timer, &onTimer, true); // edge (not level) triggered 
-  timerAlarmWrite(timer, 1000000, true); // 1000000 * 1 us = 1 s, autoreload true
-  timerAlarmEnable(timer); // enable
-}
-
-void loop() {
-}
+//hw_timer_t * timer = NULL;
+//
+//void IRAM_ATTR onTimer(){
+//  Serial.println(String(millis()));
+//}
+//
+//void setup() {
+//  Serial.begin(115200);
+//
+//  Serial.println("start timer ");
+//  timer = timerBegin(0,80,true);  // timer 0, prescaler is dividing by 80 giving time perod of 1 micro second., count up
+//  timerAttachInterrupt(timer, &onTimer, true); // edge (not level) triggered 
+//  timerAlarmWrite(timer,1000000, true); // 1000000 * 1 us = 1 s, autoreload true
+//  timerAlarmEnable(timer); // enable
+//}
+//
+//void loop() {
+//  
+//}
 
 
 
@@ -59,6 +60,7 @@ void loop() {
 //}
 //
 //void loop() {
+//  
 //}
 
 
@@ -69,14 +71,10 @@ void loop() {
 
 
 // Multiple timers
-
-
-
-
-
-
-
-
+//
+//
+//
+//
 //#include "Freenove_WS2812_Lib_for_ESP32.h"
 //
 //#define LEDS_COUNT 6
@@ -150,10 +148,10 @@ void loop() {
 //        delay(2);
 //        }  
 //}
-
-
-
-
+//
+//
+//
+//
 
 
 
@@ -162,45 +160,43 @@ void loop() {
 
 
 
-/*
+//
+//
+//
+//// the number of the LED pin
+//const int ledPin = 4;  
+//
+//// setting PWM properties
+//const int freq = 5000;
+//const int ledChannel = 0;
+//const int resolution = 8;
+// 
+//void setup(){
+//  // configure LED PWM functionalitites
+//  ledcSetup(ledChannel, freq, resolution);
+//  
+//  // attach the channel to the GPIO to be controlled
+//  ledcAttachPin(ledPin, ledChannel);
+//}
+// 
+//void loop(){
+//  // increase the LED brightness
+//  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+//    // changing the LED brightness with PWM
+//    ledcWrite(ledChannel, dutyCycle);
+//    delay(15);
+//  }
+//
+//  // decrease the LED brightness
+//  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+//    // changing the LED brightness with PWM
+//    ledcWrite(ledChannel, dutyCycle);   
+//    delay(15);
+//  }
+//}
+//
 
 
-
-
-// the number of the LED pin
-const int ledPin = 4;  
-
-// setting PWM properties
-const int freq = 5000;
-const int ledChannel = 0;
-const int resolution = 8;
- 
-void setup(){
-  // configure LED PWM functionalitites
-  ledcSetup(ledChannel, freq, resolution);
-  
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(ledPin, ledChannel);
-}
- 
-void loop(){
-  // increase the LED brightness
-  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
-    // changing the LED brightness with PWM
-    ledcWrite(ledChannel, dutyCycle);
-    delay(15);
-  }
-
-  // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
-    // changing the LED brightness with PWM
-    ledcWrite(ledChannel, dutyCycle);   
-    delay(15);
-  }
-}
-
-
-*/
 
 
 
@@ -208,32 +204,25 @@ void loop(){
 
 
 
-/*
- 
-int freq = 5000;
-int channel = 0;
-int resolution = 8;
-  
-void setup() {
-  
-  Serial.begin(115200);
-  ledcSetup(channel, freq, resolution);
-  ledcAttachPin(27, channel);
-  
-}
-  
-void loop() {
-  
-  
-  for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle=dutyCycle+10){
-  
-    Serial.println(dutyCycle);
-    ledcWrite(channel, dutyCycle);
-    delay(1000);
-  }
-  
-  
-  
-}
 
-*/  
+ 
+//int freq = 5000;
+//int channel = 0;
+//int resolution = 8;
+//  
+//void setup() {
+//  
+//
+//  ledcSetup(channel, freq, resolution);
+//  ledcAttachPin(27, channel);
+//  
+//}
+//  
+//void loop() {
+//  
+//  
+//  for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle=dutyCycle+10){
+//    ledcWrite(channel, dutyCycle);
+//    delay(1000);
+//  }
+//}
